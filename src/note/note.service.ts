6dc7760784d74fe29ae1eccdde57b5a33f53a3cb
Arangog20 <149792894+Arangog20@users.noreply.github.com>
2024-09-05 15:16:03 -0500

    creation of note creation service

--- src/note/note.service.ts
+++ src/note/note.service.ts
@@ -1,11 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateNoteDto } from './dto/create-note.dto';
 import { UpdateNoteDto } from './dto/update-note.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Note } from './entities/note.entity';
 
 @Injectable()
 export class NoteService {
-  create(createNoteDto: CreateNoteDto) {
-    return 'This action adds a new note';
+
+  constructor(@InjectModel(Note.name) private noteModel: Model<Note>){}
+
+  async create(createNoteDto: CreateNoteDto) {
+    const existingNote = await this.noteModel
+    .findOne({ _id: createNoteDto._id })
+    .exec();
+    if (existingNote) {
+      throw new HttpException (
+        ` Note with id ${createNoteDto._id} already exists`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+     const CreateNote = new this.noteModel(CreateNoteDto);
+    return CreateNote.save();
   }
 
   findAll() {
