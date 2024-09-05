6873215d5a3a3c597d6e39ea3c082c570f981950
Arangog20 <149792894+Arangog20@users.noreply.github.com>
2024-09-05 14:50:35 -0500

    create note entity

--- src/note/entities/note.entity.ts
+++ src/note/entities/note.entity.ts
@@ -1,1 +1,23 @@
-export class Note {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema()
+export class Note extends Document {
+    
+    @Prop ({required: true, type: String})
+    _id  : string;
+
+    @Prop ({required: true, type: String})
+    title: string;
+
+    @Prop ({required: true, type: String})
+    description: string;
+
+    @Prop ({required: true, type: Date})
+    createdAt : Date;
+
+    @Prop ({required: true, type: Date})
+    updatedAt : Date;
+}
+
+export const NoteSchema =
+SchemaFactory.createForClass(Note);
