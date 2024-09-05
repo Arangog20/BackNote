74e8ba376ebc7e23c8a7066d9075fcd950de8788
Arangog20 <149792894+Arangog20@users.noreply.github.com>
2024-09-05 14:57:33 -0500

    create NoteDto

--- src/note/dto/create-note.dto.ts
+++ src/note/dto/create-note.dto.ts
@@ -1,1 +1,28 @@
-export class CreateNoteDto {}
+import { 
+    IsDate,
+    IsEmpty, 
+    IsString 
+} from "class-validator";
+
+export class CreateNoteDto {
+
+    @IsEmpty()
+    @IsString()
+    _id  : string;
+
+    @IsEmpty()
+    @IsString()
+    title: string;
+
+    @IsEmpty()
+    @IsString()
+    description: string;
+
+    @IsEmpty()
+    @IsDate()
+    createdAt : Date;
+
+    @IsEmpty()
+    @IsDate()
+    updatedAt : Date;
+}
