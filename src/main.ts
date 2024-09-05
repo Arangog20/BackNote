a1f71141d70b36d2e90e81fc1e245b48f5543d1d
Arangog20 <149792894+Arangog20@users.noreply.github.com>
2024-09-04 19:54:49 -0500

    global pipes configuration

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,16 @@ async function bootstrap() {
   const port = process.env.PORT || 4000;
 
   app.setGlobalPrefix('/api/v1');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
 
   await app.listen(port);
   console.log(`Server running on port ${port}`);
