bdd91a75333962e78715f9e3cff7682a4f9bb1de
Arangog20 <149792894+Arangog20@users.noreply.github.com>
2024-09-05 19:14:38 -0500

    swagger install

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -24,8 +26,19 @@ async function bootstrap() {
       },
     }),
   );
+  const config = new DocumentBuilder()
+  .setTitle('Notapp')
+  .setDescription(
+    'notApp is a simple and efficient tool designed to help you organize your thoughts and ideas quickly and effectively. With an intuitive interface, you can easily create, edit, and delete notes, allowing you to capture and manage important information instantly. \n\n ▪️ Manuela Giraldo Arango',
+  )
+  .setVersion('1.0')
+  .addTag('Notes')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api/doc', app, document);
 
   await app.listen(port);
   console.log(`Server running on port ${port}`);
+  console.log(`Access to the project via Swagger: localhost:${port}/api-doc`);
 }
 bootstrap();
